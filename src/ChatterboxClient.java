80b04d17b04611480e815c0756ed1d0235448743
Ebtisam Jelani <93232997+Ebtisam0402@users.noreply.github.com>
2025-12-05 14:10:09 -0800

    open the sockect and connect to the server and write and read

--- src/ChatterboxClient.java
+++ src/ChatterboxClient.java
@@ -2,7 +2,10 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
@@ -161,7 +164,7 @@ public class ChatterboxClient {
         this.userOutput = userOutput;
 
          // TODO: copy options.getHost(), getPort(), getUsername(), getPassword() into fields
-
+        // when you copy the options and put into fieldset , I can access it from a different method.
         this.host = options.getHost();
         this.port = options.getPort();
         this.username = options.getUsername();
@@ -183,10 +186,25 @@ public class ChatterboxClient {
      * @throws IOException if the socket cannot be opened
      */
     public void connect() throws IOException {
-        throw new UnsupportedOperationException("Connect not yet implemented. Implement connect() and remove this exception!");
 
-        // Make sure to have this.serverReader and this.serverWriter set by the end of this method!
-        // hint: get the streams from the sockets, use those to create the InputStreamReader/OutputStreamWriter and the BufferedReader/BufferedWriter
+             // hint: get the streams from the sockets, use those to create the InputStreamReader/OutputStreamWriter and the BufferedReader/BufferedWriter
+
+            try (Socket socket = new Socket(host, port)){
+            InputStream inputStream = socket.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, java.nio.charset.StandardCharsets.UTF_8);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            OutputStream outputStream = socket.getOutputStream();
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream,java.nio.charset.StandardCharsets.UTF_8 );
+            BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+
+            // Make sure to have this.serverReader and this.serverWriter set by the end of this method!
+            this.serverReader = bufferedReader;
+            this.serverWriter = bufferedWriter;
+
+        }   
+     
+       
     }
 
     /**
